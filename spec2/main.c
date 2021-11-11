4d0065c8a55781bea8f815b613feae482ef053e9
shiridikumar <p.shiridi@research.iiit.ac.in>
2021-11-11 19:33:39 +0530

    condiitional waits

--- spec2/main.c
+++ spec2/main.c
@@ -16,9 +16,9 @@ int num_groups;
 int *grp_num;
 int spec_exit = 0;
 int goals_done = 0;
-int done_h=0;
-int done_n=0;
-int done_a=0;
+int done_h = 0;
+int done_n = 0;
+int done_a = 0;
 pthread_mutex_t zone_mutex_h;
 pthread_mutex_t zone_mutex_a;
 pthread_mutex_t zone_mutex_n;
@@ -92,17 +92,20 @@ int time_now()
 int buy_H_ticket()
 {
     pthread_mutex_lock(&zone_mutex_h);
+    done_h=0;
     int flag = 0;
     if (ch > 0)
     {
         ch -= 1;
-        done_h=1;
+        done_h = 1;
         pthread_cond_signal(&h);
         pthread_mutex_unlock(&zone_mutex_h);
         return 1;
     }
     else
     {
+        done_h=1;
+        pthread_cond_signal(&h);
         pthread_mutex_unlock(&zone_mutex_h);
         return 0;
     }
@@ -110,13 +113,13 @@ int buy_H_ticket()
 
 int buy_N_ticket()
 {
-     pthread_mutex_lock(&zone_mutex_n);
+    pthread_mutex_lock(&zone_mutex_n);
     int flag = 0;
     if (cn > 0)
     {
         cn -= 1;
-        done_n=1;
-        pthread_cond_signal(&n);
+        done_n = 1;
+        // pthread_cond_signal(&n);
         pthread_mutex_unlock(&zone_mutex_n);
         return 1;
     }
@@ -134,10 +137,10 @@ int buy_A_ticket()
     if (ca > 0)
     {
         ca -= 1;
-        done_a=1;
-        pthread_cond_signal(&a);
+        done_a = 1;
+        // pthread_cond_signal(&a);
         pthread_mutex_unlock(&zone_mutex_a);
-        done_a=0;
+        done_a = 0;
         return 1;
     }
     else
@@ -149,58 +152,65 @@ int buy_A_ticket()
 
 void spec_arrived(int i)
 {
-    for(int i=0;i<10000;i++){}
     printf("time =%d, %s has reached the staidum\n", (int)time_now(), sp[i].name);
     int a;
-    printf("%d %d %d\n",done_a,done_h,done_n);
+    // printf("%d %d %d\n",done_a,done_h,done_n);
     if (strcmp(sp[i].zone, "H") == 0)
     {
-            printf("%d %d %d\n",done_a,done_h,done_n);
-        if(done_h==0){
-            printf("blocking thread\n");
-            pthread_cond_wait(&h,&zone_mutex_h);
-            printf("unblocked\n");
+        // printf("%d %d %d\n",done_a,done_h,done_n);
+        if (done_h == 0){
+            pthread_mutex_lock(&zone_mutex_h);
+            printf("blocking thread %d\n", i);
+            pthread_cond_wait(&h, &zone_mutex_h);
+            printf("unblocked %d\n", i);
+            pthread_mutex_unlock(&zone_mutex_h);
         }
         a = buy_H_ticket();
         if (a == 0)
         {
+
             a = buy_N_ticket();
             if (a == 0)
             {
                 printf("time =%d, %s could ot get a seat\n", (int)time_now(), sp[i].name);
                 sp[i].wait_start = (int)time_now();
-
             }
             else
             {
                 printf("time =%d, %s has got a seat in zone %s\n", (int)time_now(), sp[i].name, "N");
-                sp[i].got_seat=1;
+                sp[i].got_seat = 1;
             }
         }
         else
         {
             printf("time =%d, %s has got a seat in zone %s\n", (int)time_now(), sp[i].name, "H");
-            sp[i].got_seat=1;
+            sp[i].got_seat = 1;
         }
     }
 
     if (strcmp(sp[i].zone, "N") == 0)
     {
-        if(done_n==0){
-            printf("waited for zoen a mutex\n");
-            pthread_mutex_lock(&zone_mutex_n);
-            pthread_cond_wait(&a,&zone_mutex_a);
-            printf("unblcoked thread after");
-        }
+        //if(done_n==0){
+        //  pthread_cond_wait(&a,&zone_mutex_a);
+        //}
         a = buy_N_ticket();
         if (a == 0)
         {
-            if(done_a==0){
+            /* if(done_a==0){
                 pthread_cond_wait(&a,&zone_mutex_a);
+            }*/
+            if (done_h == 0)
+            {
+                pthread_mutex_lock(&zone_mutex_h);
+                printf("blocking thread %d\n", i);
+                pthread_cond_wait(&h, &zone_mutex_h);
+                printf("unblocked %d\n", i);
+                pthread_mutex_unlock(&zone_mutex_h);
             }
             a = buy_H_ticket();
             if (a == 0)
             {
+                
                 a = buy_A_ticket();
                 if (a == 0)
                 {
@@ -210,19 +220,19 @@ void spec_arrived(int i)
                 else
                 {
                     printf("time =%d, %s has got a seat in zone %s\n", (int)time_now(), sp[i].name, "A");
-                    sp[i].got_seat=1;
+                    sp[i].got_seat = 1;
                 }
             }
             else
             {
                 printf("time =%d, %s has got a seat in zone %s\n", (int)time_now(), sp[i].name, "H");
-                sp[i].got_seat=1;
+                sp[i].got_seat = 1;
             }
         }
         else
         {
             printf("time =%d, %s has got a seat in zone %s\n", (int)time_now(), sp[i].name, "N");
-            sp[i].got_seat=1;
+            sp[i].got_seat = 1;
         }
     }
 
@@ -237,10 +247,9 @@ void spec_arrived(int i)
         else
         {
             printf("time =%d, %s has got a seat in zone %s\n", (int)time_now(), sp[i].name, "A");
-            sp[i].got_seat=1;
+            sp[i].got_seat = 1;
         }
     }
-
 }
 
 void start_simulation()
@@ -335,7 +344,7 @@ int main()
             scanf("%s %s %d %d %d", sp[ind].name, sp[ind].zone, &sp[ind].time_entered, &sp[ind].patience, &sp[ind].num_goals);
             sp[ind].grp_num = i;
             sp[ind].entered = 0;
-            sp[ind].got_seat=0;
+            sp[ind].got_seat = 0;
             ind++;
         }
         total += grp_num[i];
