e1f58ac01d974349e5f74b34895372ef736b6907
shiridikumar <p.shiridi@research.iiit.ac.in>
2021-11-24 10:35:07 +0530

    updated spec 1

--- main.c
+++ main.c
@@ -27,6 +27,7 @@ typedef struct{
     int found;
 }courses;
 courses *cs;
+int *cow;
 typedef struct{
     float calibre;
     int pref_1;
@@ -64,6 +65,7 @@ pthread_mutex_t cslock;
 pthread_t *cour_threads; 
 int *probs;
 sem_t *stut;
+sem_t *try;
 int split(char *c, char **arr)
 {
     int coun = 0;
@@ -85,31 +87,37 @@ int split(char *c, char **arr)
 
 int student_filled(int i){
     printf("student %d has filled in preferences for course registration\n",i);
-    int pref_1=st[i].pref_1;
-    int prob1 =(rand()%cs[pref_1].max_slots)+1;
-    pthread_mutex_lock(&cour_locks[pref_1]);
-    if(cou[pref_1]!=1){
-        cou[pref_1]=1;
-        probs[pref_1]=prob1;
-        pthread_mutex_unlock(&cour_locks[pref_1]);
-        sem_post(&tut[pref_1]);
-        sem_wait(&stut[pref_1]);
+    while(st[i].done!=1){
+        int pref_1=st[i].pref_1;
+        int prob1 =(rand()%cs[pref_1].max_slots)+1;
+        pthread_mutex_lock(&cour_locks[pref_1]);
+        if(cou[pref_1]!=1){
+            cou[pref_1]=1;
+            probs[pref_1]=prob1;
+            pthread_mutex_unlock(&cour_locks[pref_1]);
+            sem_post(&tut[pref_1]);
+            sem_wait(&stut[pref_1]);
+        }
+        else{
+            pthread_mutex_unlock(&cour_locks[pref_1]);
+        }
+        pthread_mutex_lock(&cour_locks[pref_1]);
+        if(cs[pref_1].curr_slots<probs[pref_1] && cs[pref_1].found==1){
+            cs[pref_1].curr_slots++;
+            printf(GREEN "student %d has been allocated a seat in course %s\n" RESET,i,cs[pref_1].course_name);
+            pthread_mutex_unlock(&cour_locks[pref_1]);
+            st[i].done=1;
+        }
+        else{
+            pthread_mutex_unlock(&cour_locks[pref_1]);
+            cow[pref_1]++;
+            sem_wait(&try[pref_1]);
+            student_filled(i);
+            printf( RED "fuck off there is an error in code %d  %s curr %d\n" RESET,i,cs[pref_1].course_name,cs[pref_1].curr_slots);
+        } 
+    }
+    
 
-    }
-    else{
-        pthread_mutex_unlock(&cour_locks[pref_1]);
-    }
-    pthread_mutex_lock(&cour_locks[pref_1]);
-    if(cs[pref_1].curr_slots<probs[pref_1] && cs[pref_1].found==1){
-        cs[pref_1].curr_slots++;
-        printf(GREEN "student %d has been allocated a seat in course %s\n" RESET,i,cs[pref_1].course_name);
-        pthread_mutex_unlock(&cour_locks[pref_1]);
-        st[i].done=1;
-    }
-    else{
-        pthread_mutex_unlock(&cour_locks[pref_1]);
-        printf( RED "fuck off there is an error in code %d  %s curr %d\n" RESET,i,cs[pref_1].course_name,cs[pref_1].curr_slots);
-    } 
 
 }
 
@@ -119,13 +127,16 @@ int time_now(){
 
 int conduct_tut(int ind){
     sem_wait(&tut[ind]);
-    for(int i=0;i<cs[ind].num_labs;i++){
+    sem_wait(&try[ind]);
+    int i,j;
+    for(i=0;i<cs[ind].num_labs;i++){
         pthread_mutex_lock(&lblock[cs[ind].lab_ids[i]]);
-        for(int j=0;j<lb[cs[ind].lab_ids[i]].num_ta;j++){
+        for(j=0;j<lb[cs[ind].lab_ids[i]].num_ta;j++){
             if(lb[cs[ind].lab_ids[i]].tas[j]!=lb[cs[ind].lab_ids[i]].max_num){// && lb[cs[ind].lab_ids[i]].tas_avail[j]){
                 lb[cs[ind].lab_ids[i]].tas[j]++;
                 lb[cs[ind].lab_ids[i]].tas_avail[j]=0;
                 cs[ind].found=1;
+                printf("TA %d from lab %s has been allocated to course %s for his %d'th TA ship\n",j+1,lb[cs[ind].lab_ids[i]].lab_name,cs[ind].course_name,lb[cs[ind].lab_ids[i]].tas[j]);
                 break;
             }
         }
@@ -136,9 +147,14 @@ int conduct_tut(int ind){
     printf(YELLOW "course %s has been allocated %d seats\n" RESET,cs[ind].course_name,probs[ind]);
     sem_post(&stut[ind]);
     sleep(1);
-    pthread_mutex_lock(&cour_locks[ind]);
     printf(BLUE "Tutorial has been started for course %s with %d seats filled out of %d\n" RESET,cs[ind].course_name,cs[ind].curr_slots,probs[ind]);
+    pthread_mutex_lock(&cour_locks[ind]);
+    sleep(2);
+    cs[ind].found=0;
+    lb[cs[ind].lab_ids[i]].tas_avail[j]=1;
+    cs[ind].curr_slots=0;
     pthread_mutex_unlock(&cour_locks[ind]);
+    sem_post(&try[ind]);
 }
 
 int fill_slots(){
@@ -193,20 +209,24 @@ int main()
     }
     tut=(sem_t *)malloc(num_courses*sizeof(sem_t));
     stut=(sem_t *)malloc(num_courses*sizeof(sem_t));
+    try=(sem_t *)malloc(num_courses*sizeof(sem_t));
     cour_locks=(pthread_mutex_t *)malloc(num_courses*sizeof(pthread_mutex_t));
     crm=(int *)malloc(num_courses*sizeof(int));
+    cow=(int *)malloc(num_courses*sizeof(int));
     cou=(int *)malloc(num_courses*sizeof(int));
     cour_threads=(pthread_t *)malloc(num_courses*sizeof(pthread_t));
 
 
     for(int i=0;i<num_courses;i++){
         sem_init(&tut[i],0,1);
+        sem_init(&try[i],0,1);
         sem_init(&stut[i],0,1);
         sem_wait(&tut[i]);
         sem_wait(&stut[i]);
        pthread_create(&cour_threads[i],NULL,conduct_tut,(i));
        cs[i].curr_slots=0;
        cs[i].found=0;
+       cow[i]=0;
        cou[i]=0;
     }
 
@@ -215,7 +235,6 @@ int main()
         scanf("%f %d %d %d %f",&st[i].calibre,&st[i].pref_1,&st[i].pref_2,&st[i].pref_3,&st[i].time);
         st[i].filled=0;
         st[i].done=0;
-    
     }
 
     probs=(int *)malloc(num_courses*sizeof(int));
